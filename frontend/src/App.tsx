427b1940701f67db4220860ae98772cd633f3ff2
Breno Moura <breno.mm@outlook.com>
2023-12-26 23:39:15 -0300

    change: change to use mantine ui instead of material ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from './routes';
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
 
 function App() {
   return (
-    <>
+    <MantineProvider defaultColorScheme="dark">
       <RouterProvider router={router} />
-    </>
+    </MantineProvider>
   )
 }
 
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,22 +1,14 @@
-import Grid from "@mui/material/Grid"
 import Discussions from "../components/home/Discussions"
 import FeaturedProductList from "../components/home/FeaturedProductList"
 import SearchBar from "../components/shared/SearchBar"
 
 const Home = () => {
-// Todo : make it responsive for smartphones
+    // Todo : make it responsive for smartphones
     return (
         <div>
-            
             <SearchBar />
-            <Grid container spacing={2}>
-                <Grid item xs={6}>
-                    <Discussions />
-                </Grid>
-                <Grid item xs={6}>
-                    <FeaturedProductList />
-                </Grid>
-            </Grid>
+            <Discussions />
+            <FeaturedProductList />
         </div>
     )
 }
